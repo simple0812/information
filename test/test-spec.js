7ad018cd2da80bdf0c0c696b4fe4a0f78ca0d686
simple0812 <simple0812@sina.cn>
2015-08-05 17:16:17 +0800

    merge

--- test/test-spec.js
+++ test/test-spec.js
@@ -2,6 +2,35 @@ var request = require('request');
 var host = 'http://localhost:3004';
 var proxy = require('../proxy');
 
+var frisby = require('frisby');
+
+
+frisby.create('GET user johndoe')
+  .get(host + '/users')
+  .expectStatus(200)
+  .expectJSONTypes({
+    id: Number,
+    username: String,
+    is_admin: Boolean
+  })
+  .expectJSON({
+    id: 3,
+    username: 'johndoe',
+    is_admin: false
+  })
+  // 'afterJSON' automatically parses response body as JSON and passes it as an argument 
+  .afterJSON(function(user) {
+    // You can use any normal jasmine-style assertions here 
+    expect(1+1).toEqual(2);
+ 
+    // Use data from previous result in next test 
+    frisby.create('Update user')
+      .put(URL_AUTH + '/users/' + user.id + '.json', {tags: ['jasmine', 'bdd']})
+      .expectStatus(200)
+    .toss();
+  })
+.toss();
+
 describe("api接口测试", function() {
   it("/users 获取用户列表", function(done) {
     var opt = {
